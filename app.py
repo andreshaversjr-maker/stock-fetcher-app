36d78359f4f9297a6ee02e87ee8ff5babfc10987
andreshaversjr-maker <andreshaversjr@gmail.com>
2025-11-25 12:38:18 -0800

    app.py

--- app.py
+++ app.py
@@ -29,5 +29,8 @@ result[t] = {
 }
 return jsonify(result)
 
+import os
+
 if __name__ == "__main__":
-app.run(host="0.0.0.0", port=8080)
+port = int(os.environ.get("PORT", 8080)) # Use Cloud Run's assigned port
+app.run(host="0.0.0.0", port=port)
